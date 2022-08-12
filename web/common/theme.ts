9896e71dd4faaf9ae1ba0bbdf90d90efabdab3e2
Joel Keyser <keyser.joel@gmail.com>
2022-08-12 11:48:03 -0500

    Add dark theme, extract theme to file

---
+++ web/common/theme.ts
@@ -0,0 +1,35 @@
+// used material color palettes to find complement https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+// used material color tool to confirm accessibility (legibility) https://material.io/resources/color/#!/?view.left=1&view.right=1&primary.color=4ab74c&secondary.color=b74ab5
+// used mui-theme-creator to see the colors in action and pick light & dark palettes https://bareynol.github.io/mui-theme-creator/?firstName=&lastName=&email=&password=
+
+import { PaletteMode } from "@mui/material";
+
+const lightPalette = {
+  background: {
+    default: "#fafafa",
+  },
+};
+
+const darkPalette = {
+  background: {
+    default: "#303030",
+    paper: "rgb(35, 35, 35)",
+  },
+};
+
+const sharedPalette = {
+  primary: {
+    main: "#4ab74c", // green: good, optimistic, let's solve things
+  },
+  secondary: {
+    main: "#b84ab6", // purple: truth, complementary to green
+  },
+};
+
+export const getThemeOptions = (mode: PaletteMode) => ({
+  palette: {
+    mode,
+    ...sharedPalette,
+    ...(mode === "light" ? lightPalette : darkPalette),
+  },
+});
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,23 +2,11 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-
-const theme = createTheme({
-  // used material color palettes to find complement https://material.io/design/color/the-color-system.html#tools-for-picking-colors
-  // used material color tool to confirm accessibility (legibility) https://material.io/resources/color/#!/?view.left=1&view.right=1&primary.color=4ab74c&secondary.color=b74ab5
-  // used mui-theme-creator to see the colors in action https://bareynol.github.io/mui-theme-creator/?firstName=&lastName=&email=&password=
-  palette: {
-    mode: "light",
-    primary: {
-      main: "#4ab74c", // green: good, optimistic, let's solve things
-    },
-    secondary: {
-      main: "#b84ab6", // purple: truth, complementary to green
-    },
-  },
-});
+import { getThemeOptions } from "../common/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createTheme(getThemeOptions("dark"));
+
   return (
     <>
       <ThemeProvider theme={theme}>
