c151ee603c5cfe200562c434ae96e82cf0a088bd
Joel Keyser <keyser.joel@gmail.com>
2022-08-26 09:56:44 -0500

    Build home via MUI

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,62 +1,45 @@
-import { useTheme } from "@mui/material";
+import { Box, Divider, Paper, Typography } from "@mui/material";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  const theme = useTheme();
-
   return (
     <>
-      <div className="center">
-        <section className="paper">
-          <h2>No problem is too complex.</h2>
-          <h2>Solving made possible. For anyone.</h2>
-          <h2>Disagree based on values, not on miscommunication.</h2>
-          <h2>Understand yourself. Understand each other. Grow together.</h2>
+      <Box display="flex" justifyContent="center">
+        <Paper sx={paper}>
+          <Typography variant="h2">Don&#39;t sit back any longer.</Typography>
+          <Typography variant="h5">Understand yourself.</Typography>
+          <Typography variant="h5">Understand each other.</Typography>
+          <Typography variant="h5">Grow together.</Typography>
 
-          <div className="split">
-            <div>
-              <h2>Hot</h2>
-              <p>Racism</p>
-              <p>School Shootings</p>
-            </div>
-            <div>
-              <h2>New</h2>
-              <p>Climate Change</p>
-            </div>
-            <div>
-              <h2>Recently Visited</h2>
-              <p>World Hunger</p>
-              <p>School Shootings</p>
-            </div>
-          </div>
-        </section>
-      </div>
+          <Divider />
 
-      <style jsx>{`
-        .center {
-          display: flex;
-          justify-content: center;
-        }
-
-        .paper {
-          background-color: ${theme.palette.background.paper};
-          width: 1080px;
-          margin: 10px;
-          padding: 10px;
-          text-align: center;
-        }
-
-        .split {
-          display: flex;
-          border-top: solid ${theme.palette.divider};
-        }
-
-        .split * {
-          flex: 1 1 0px;
-        }
-      `}</style>
+          <Box display="flex">
+            <Box flex="1">
+              <Typography variant="h5">Hot</Typography>
+              <Typography variant="body1">Racism</Typography>
+              <Typography variant="body1">School Shootings</Typography>
+            </Box>
+            <Box flex="1">
+              <Typography variant="h5">New</Typography>
+              <Typography variant="body1">Climate Change</Typography>
+            </Box>
+            <Box flex="1">
+              <Typography variant="h5">Recently Visited</Typography>
+              <Typography variant="body1">World Hunger</Typography>
+              <Typography variant="body1">School Shootings</Typography>
+            </Box>
+          </Box>
+        </Paper>
+      </Box>
     </>
   );
+};
+
+const paper = {
+  width: "1080px",
+  textAlign: "center",
+  margin: "10px",
+  padding: "10px",
 };
 
 export default Home;
