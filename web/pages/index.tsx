08f3639aa874fb81674b83d8266a3ee3579dd113
Joel Keyser <keyser.joel@gmail.com>
2022-08-31 07:58:34 -0500

    Add empty text to react flow

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import _ from "lodash";
 import { NextPage } from "next";
 import { useState } from "react";
 import ReactFlow, {
@@ -18,15 +19,21 @@ const initialNodes = [
 
 const Home: NextPage = () => {
   const [nodes] = useState<Node[]>(initialNodes);
+  // const [nodes] = useState<Node[]>();
   const [edges] = useState<Edge[]>();
+
+  const emptyText = <Typography variant="h5">Right-click to create</Typography>;
 
   return (
     <Box width="100%" height="100%">
-      <ReactFlow nodes={nodes} edges={edges} fitView>
+      <ReactFlow nodes={nodes} edges={edges} fitView style={centerText}>
         <Background variant={BackgroundVariant.Dots} />
+        {_(nodes).isEmpty() && emptyText}
       </ReactFlow>
     </Box>
   );
 };
+
+const centerText = { display: "flex", justifyContent: "center", alignItems: "center" };
 
 export default Home;
