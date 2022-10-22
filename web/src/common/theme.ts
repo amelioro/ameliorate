075922887a930ac604065b43f9e389eb67d8769f
Joel Keyser <keyser.joel@gmail.com>
2022-10-22 18:11:14 -0500

    feat: add problem and solution colors to theme

--- web/src/common/theme.ts
+++ web/src/common/theme.ts
@@ -2,7 +2,27 @@
 // used material color tool to confirm accessibility (legibility) https://material.io/resources/color/#!/?view.left=1&view.right=1&primary.color=4ab74c&secondary.color=b74ab5
 // used mui-theme-creator to see the colors in action and pick light & dark palettes https://bareynol.github.io/mui-theme-creator/?firstName=&lastName=&email=&password=
 
-import { type PaletteMode, type ThemeOptions } from "@mui/material";
+import { type PaletteMode, type ThemeOptions, createTheme } from "@mui/material";
+
+// adding colors to theme documented at https://mui.com/material-ui/customization/palette/#adding-new-colors
+declare module "@mui/material/styles" {
+  interface Palette {
+    problem: Palette["primary"];
+    solution: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    problem: PaletteOptions["primary"];
+    solution: PaletteOptions["primary"];
+  }
+}
+
+declare module "@mui/material/Button" {
+  interface ButtonPropsColorOverrides {
+    problem: true;
+    solution: true;
+  }
+}
 
 const lightPalette = {
   // background: {
@@ -17,13 +37,20 @@ const darkPalette = {
   // },
 };
 
+const primary = "#4ab74c";
+const secondary = "#b84ab6"; // slightly darker so that text isn't white?
+const { palette } = createTheme();
+const { augmentColor } = palette; // automatically creates light and dark colors too, thanks https://stackoverflow.com/a/69836010
+
 const sharedPalette = {
   primary: {
-    main: "#4ab74c", // green: good, optimistic, let's solve things
+    main: primary, // green: good, optimistic, let's solve things
   },
   secondary: {
-    main: "#b84ab6", // purple: truth, complementary to green
+    main: secondary, // purple: truth, complementary to green
   },
+  problem: augmentColor({ color: { main: secondary } }),
+  solution: augmentColor({ color: { main: primary } }),
 };
 
 export const getThemeOptions = (mode: PaletteMode): ThemeOptions => ({
