9af0ff14cd1941fefab4019564e487da3481628c
Joel Keyser <keyser.joel@gmail.com>
2022-12-03 16:05:41 -0600

    refactor(store): add immer, unused

--- web/src/modules/topic/store/store.ts
+++ web/src/modules/topic/store/store.ts
@@ -1,5 +1,6 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
 import { NodeType } from "../components/nodeDecorations";
 import { Direction, layout } from "../utils/layout";
@@ -97,124 +98,127 @@ interface DiagramActions {
 // seems to report all lines in the invocation, making it very hard to debug.
 export const useDiagramStore = create<AllDiagramState & DiagramState & DiagramActions>()(
   // seems like we should be able to auto-wrap all stores with devtools
-  devtools((set) => ({
-    activeDiagramId: "root",
-    rootDiagramId: "root",
-    claimDiagramIds: [],
-    nodes: diagrams.root.nodes,
-    edges: diagrams.root.edges,
-    direction: diagrams.root.direction,
 
-    addNode: (toNodeId, as, type) => {
-      set(
-        (state) => {
-          const toNode = state.nodes.find((node) => node.id === toNodeId);
-          if (!toNode) throw new Error("toNode not found");
+  immer(
+    devtools((set, get) => ({
+      activeDiagramId: "root",
+      rootDiagramId: "root",
+      claimDiagramIds: [],
+      nodes: diagrams.root.nodes,
+      edges: diagrams.root.edges,
+      direction: diagrams.root.direction,
 
-          const newNodeId = nextNodeId();
-          const newNode = buildNode({
-            id: newNodeId,
-            type: type,
-          });
+      addNode: (toNodeId, as, type) => {
+        set(
+          (state) => {
+            const toNode = state.nodes.find((node) => node.id === toNodeId);
+            if (!toNode) throw new Error("toNode not found");
 
-          const sourceNodeId = as === "Parent" ? newNodeId : toNodeId;
-          const targetNodeId = as === "Parent" ? toNodeId : newNodeId;
-          const newEdge = buildEdge(sourceNodeId, targetNodeId);
+            const newNodeId = nextNodeId();
+            const newNode = buildNode({
+              id: newNodeId,
+              type: type,
+            });
 
-          const newNodes = state.nodes.concat(newNode);
-          const newEdges = state.edges.concat(newEdge);
-          const { layoutedNodes, layoutedEdges } = layout(newNodes, newEdges, state.direction);
-          return { nodes: layoutedNodes, edges: layoutedEdges };
-        },
-        false,
-        "addNode" // little gross, seems like this should be inferrable from method name
-      );
-    },
+            const sourceNodeId = as === "Parent" ? newNodeId : toNodeId;
+            const targetNodeId = as === "Parent" ? toNodeId : newNodeId;
+            const newEdge = buildEdge(sourceNodeId, targetNodeId);
 
-    deselectNodes: () => {
-      set(
-        (state) => {
-          const newNodes = state.nodes.map((node) => {
-            return { ...node, selected: false };
-          });
-          return { nodes: newNodes };
-        },
-        false,
-        "deselectNodes"
-      );
-    },
+            const newNodes = state.nodes.concat(newNode);
+            const newEdges = state.edges.concat(newEdge);
+            const { layoutedNodes, layoutedEdges } = layout(newNodes, newEdges, state.direction);
+            return { nodes: layoutedNodes, edges: layoutedEdges };
+          },
+          false,
+          "addNode" // little gross, seems like this should be inferrable from method name
+        );
+      },
 
-    doesDiagramExist: doesDiagramExist,
+      deselectNodes: () => {
+        set(
+          (state) => {
+            const newNodes = state.nodes.map((node) => {
+              return { ...node, selected: false };
+            });
+            return { nodes: newNodes };
+          },
+          false,
+          "deselectNodes"
+        );
+      },
 
-    // will this trigger re-render for all parentType components, since Diagram depends on the whole array?
-    // theoretically we should be able to just re-render the affected component...
-    // at least the HTML should mostly be unchanged I guess; not sure how big of a deal the performance impact is here
-    scoreParent: (parentId, parentType, score) => {
-      // immer seems like it'd make this a lot simpler https://github.com/pmndrs/zustand#sick-of-reducers-and-changing-nested-state-use-immer
-      // state[parentsKey].data.score = score;
-      set(
-        (state) => {
-          const parentsKey = parentType === "node" ? "nodes" : "edges";
-          const parents = state[parentsKey];
-          const newParents = parents.map((parent) => {
-            if (parent.id === parentId) {
-              return { ...parent, data: { ...parent.data, score: score } };
-            }
-            return parent;
-          });
-          return { [parentsKey]: newParents };
-        },
-        false,
-        "scoreParent"
-      );
-    },
+      doesDiagramExist: doesDiagramExist,
 
-    setActiveDiagram: (diagramId) => {
-      set(
-        (state) => {
-          // save current diagram state before switching
-          // TODO: perhaps we could use classes to isolate/indicate state & state change?
-          /* eslint-disable functional/immutable-data */
-          diagrams[state.activeDiagramId].nodes = state.nodes;
-          diagrams[state.activeDiagramId].edges = state.edges;
-          /* eslint-enable functional/immutable-data */
+      // will this trigger re-render for all parentType components, since Diagram depends on the whole array?
+      // theoretically we should be able to just re-render the affected component...
+      // at least the HTML should mostly be unchanged I guess; not sure how big of a deal the performance impact is here
+      scoreParent: (parentId, parentType, score) => {
+        // immer seems like it'd make this a lot simpler https://github.com/pmndrs/zustand#sick-of-reducers-and-changing-nested-state-use-immer
+        // state[parentsKey].data.score = score;
+        set(
+          (state) => {
+            const parentsKey = parentType === "node" ? "nodes" : "edges";
+            const parents = state[parentsKey];
+            const newParents = parents.map((parent) => {
+              if (parent.id === parentId) {
+                return { ...parent, data: { ...parent.data, score: score } };
+              }
+              return parent;
+            });
+            return { [parentsKey]: newParents };
+          },
+          false,
+          "scoreParent"
+        );
+      },
 
-          // create new diagram if it doesn't exist
-          if (!doesDiagramExist(diagramId)) {
+      setActiveDiagram: (diagramId) => {
+        set(
+          (state) => {
+            // save current diagram state before switching
             // TODO: perhaps we could use classes to isolate/indicate state & state change?
-            // eslint-disable-next-line functional/immutable-data
-            diagrams[diagramId] = {
-              nodes: getInitialNodes("RootClaim"),
-              edges: [],
-              direction: "LR",
-            };
+            /* eslint-disable functional/immutable-data */
+            diagrams[get().activeDiagramId].nodes = get().nodes; // get() because diagrams exist outside of this immer'd method and should not take draft state outside of this scope
+            diagrams[get().activeDiagramId].edges = get().edges;
+            /* eslint-enable functional/immutable-data */
 
-            const claimDiagramIds = state.claimDiagramIds.concat(diagramId);
-            return { activeDiagramId: diagramId, ...diagrams[diagramId], claimDiagramIds };
-          }
+            // create new diagram if it doesn't exist
+            if (!doesDiagramExist(diagramId)) {
+              // TODO: perhaps we could use classes to isolate/indicate state & state change?
+              // eslint-disable-next-line functional/immutable-data
+              diagrams[diagramId] = {
+                nodes: getInitialNodes("RootClaim"),
+                edges: [],
+                direction: "LR",
+              };
 
-          // set diagram
-          return { activeDiagramId: diagramId, ...diagrams[diagramId] };
-        },
-        false,
-        "setActiveDiagram"
-      );
-    },
-
-    setNodeLabel: (nodeId, value) => {
-      set(
-        (state) => {
-          const newNodes = state.nodes.map((node) => {
-            if (node.id === nodeId) {
-              return { ...node, data: { ...node.data, label: value } };
+              const claimDiagramIds = state.claimDiagramIds.concat(diagramId);
+              return { activeDiagramId: diagramId, ...diagrams[diagramId], claimDiagramIds };
             }
-            return node;
-          });
-          return { nodes: newNodes };
-        },
-        false,
-        "setNodeLabel"
-      );
-    },
-  }))
+
+            // set diagram
+            return { activeDiagramId: diagramId, ...diagrams[diagramId] };
+          },
+          false,
+          "setActiveDiagram"
+        );
+      },
+
+      setNodeLabel: (nodeId, value) => {
+        set(
+          (state) => {
+            const newNodes = state.nodes.map((node) => {
+              if (node.id === nodeId) {
+                return { ...node, data: { ...node.data, label: value } };
+              }
+              return node;
+            });
+            return { nodes: newNodes };
+          },
+          false,
+          "setNodeLabel"
+        );
+      },
+    }))
+  )
 );
