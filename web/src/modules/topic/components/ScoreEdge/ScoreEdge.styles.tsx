057866be6539b9ceb7db9dcc6c5363334a994b63
Joel Keyser <keyser.joel@gmail.com>
2023-03-29 14:13:03 -0500

    feat: fade implied edges

--- web/src/modules/topic/components/ScoreEdge/ScoreEdge.styles.tsx
+++ web/src/modules/topic/components/ScoreEdge/ScoreEdge.styles.tsx
@@ -1,26 +1,58 @@
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
+interface PathProps {
+  isImplied: boolean;
+}
+
+const pathOptions = {
+  shouldForwardProp: (prop: string) => !["isImplied"].includes(prop),
+};
+
+export const StyledPath = styled("path", pathOptions)<PathProps>`
+  ${({ isImplied }) => {
+    if (isImplied) {
+      return css`
+        opacity: 0.5;
+      `;
+    }
+  }}
+`;
+
 interface DivProps {
   labelX: number;
   labelY: number;
+  isImplied: boolean;
 }
 
-export const StyledDiv = styled.div<DivProps>`
+const divOptions = {
+  shouldForwardProp: (prop: string) => !["labelX", "labelY", "isImplied"].includes(prop),
+};
+
+export const StyledDiv = styled("div", divOptions)<DivProps>`
+  pointer-events: all;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: white;
+  border: 1px solid #b1b1b7; // match edge stroke color that comes from react-flow default css
+  border-radius: 15%;
+  padding: 4px;
+
   ${({ labelX, labelY }) => css`
     position: absolute;
     transform: translate(-50%, -50%) translate(${labelX}px, ${labelY}px);
-
-    pointer-events: all;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    background-color: white;
-    border: 1px solid #b1b1b7; // match edge stroke color that comes from react-flow default css
-    border-radius: 15%;
-    padding: 4px;
   `}
+
+  ${({ isImplied }) => {
+    if (isImplied) {
+      return css`
+        opacity: 0.5;
+        z-index: -1; // behind path svg which has z-index: 0
+      `;
+    }
+  }}
 `;
