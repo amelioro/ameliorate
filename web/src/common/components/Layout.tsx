d44e8621480591b434c2c6aefd8f2c43b52c7321
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2023-04-28 00:40:47 +0530

    chore: add facebook icon on navbar (#130)
    * chore: added facebook icon on navbar
    
    * chore: updated facebook icon's height on navbar

--- web/src/common/components/Layout.tsx
+++ web/src/common/components/Layout.tsx
@@ -29,6 +29,9 @@ const Layout: NextPage<LayoutProps> = ({ children }) => {
     theme.palette.mode == "light" ? "/GitHub-Mark-64px.png" : "/GitHub-Mark-Light-64px.png";
   const discordIconSrc =
     theme.palette.mode == "light" ? "/Discord-Mark-Black.png" : "/Discord-Mark-White.png";
+  const facebookIconSrc =
+    theme.palette.mode == "light" ? "/Facebook-Icon-Black.png" : "/Facebook-Icon-White.png";
+
 
   return (
     <>
@@ -41,6 +44,9 @@ const Layout: NextPage<LayoutProps> = ({ children }) => {
               {/* <NavLink href="/mine">my understandings</NavLink> */}
               <NavLink href="/about">about</NavLink>
               {/* <NavLink href="/login">login</NavLink> */}
+              <NavLink href="https://www.facebook.com/profile.php?id=100091844721178" display="flex">
+                <Image src={facebookIconSrc} height={32} width={32} alt="facebook link" />
+              </NavLink>
               <NavLink href="https://discord.gg/3KhdyJkTWT" display="flex">
                 <Image src={discordIconSrc} height={24} width={32} alt="discord link" />
               </NavLink>
