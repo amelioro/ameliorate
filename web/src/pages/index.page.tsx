6d40de55e9ce869d92488c1fc4a2512cc68b14c3
Joel Keyser <keyser.joel@gmail.com>
2023-04-29 07:02:54 -0500

    feat: create home page w placeholder demo

--- web/src/pages/index.page.tsx
+++ web/src/pages/index.page.tsx
@@ -1,5 +1,11 @@
+import { ArrowDownward } from "@mui/icons-material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Link from "next/link";
+
+import { YoutubeEmbed } from "../common/components/YoutubeEmbed/YoutubeEmbed";
+import { StyledBox } from "./index.style";
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +17,43 @@ const Home: NextPage = () => {
           content="Ameliorate is a tool that makes it easier to solve tough problems by helping communicate about and mutually understand them."
         />
       </Head>
+
+      <StyledBox>
+        <Typography variant="h3" color="primary">
+          Ameliorate
+        </Typography>
+
+        <Typography variant="h5">
+          Understand ourselves. Understand each other. Grow together.
+        </Typography>
+
+        <Typography variant="body1">
+          A tool for communicating about and mutually understanding tough problems
+        </Typography>
+
+        <Box display="flex" margin="0.75rem">
+          <Link href="/solve" passHref>
+            <Button variant="contained">Solve</Button>
+          </Link>
+          <Button
+            variant="outlined"
+            endIcon={<ArrowDownward />}
+            href="#how-it-works"
+            sx={{ marginLeft: "0.5rem" }}
+          >
+            See how it works
+          </Button>
+        </Box>
+      </StyledBox>
+
+      <Divider />
+
+      <section id="how-it-works">
+        <StyledBox>
+          <Typography variant="h4">How it works</Typography>
+          <YoutubeEmbed embedId="ScMzIvxBSi4" />
+        </StyledBox>
+      </section>
     </>
   );
 };
---
+++ web/src/pages/index.style.tsx
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const StyledBox = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 5rem 0;
+  text-align: center;
+`;
