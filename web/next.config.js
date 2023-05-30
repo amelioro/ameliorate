2de3713769c384dc4c8ab5117083aae961521fb1
Joel Keyser <keyser.joel@gmail.com>
2023-05-30 09:30:31 -0500

    chore: remove unnecessary custom babel
    this enables use of next's SWC, which is supposed to make something
    faster

--- web/next.config.js
+++ web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    emotion: true,
+  },
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
   reactStrictMode: true,
   swcMinify: true,
