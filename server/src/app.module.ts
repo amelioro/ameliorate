45c367cee026caf86dd2992f9b3e2bfb4efdcfc0
Joel Keyser <keyser.joel@gmail.com>
2023-06-16 14:05:12 -0500

    chore: add open api

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersController } from './users/controller';
 import { TopicsController } from './topics/controller';
 import { UsersService } from './users/service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsersController, TopicsController],
-  providers: [AppService, UsersService],
+  controllers: [UsersController, TopicsController],
+  providers: [UsersService],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Ameliorate API')
+    .setDescription('The Ameliorate API description')
+    .setVersion('1.0')
+    .addTag('ameliorate')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
