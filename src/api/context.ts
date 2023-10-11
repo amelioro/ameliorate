3ba893d0ed3697ffb0a8fb537a9fa5b988418935
Joel Keyser <keyser.joel@gmail.com>
2023-10-11 12:46:01 -0500

    refactor(router): always add user to context

--- src/api/context.ts
+++ src/api/context.ts
@@ -2,11 +2,18 @@ import { getSession } from "@auth0/nextjs-auth0";
 import { inferAsyncReturnType } from "@trpc/server";
 import { CreateNextContextOptions } from "@trpc/server/adapters/next";
 
+import { xprisma } from "../db/extendedPrisma";
+
 export const createContext = async ({ req, res }: CreateNextContextOptions) => {
   const session = await getSession(req, res);
   const userAuthId = session?.user.sub as string | null;
   const userEmailVerified = session?.user.email_verified as boolean | null;
-  return { userAuthId, userEmailVerified };
+
+  if (!userAuthId) return { userAuthId, userEmailVerified };
+
+  const user = await xprisma.user.findFirst({ where: { authId: userAuthId } });
+
+  return { userAuthId, userEmailVerified, user };
 };
 
 export type Context = inferAsyncReturnType<typeof createContext>;
