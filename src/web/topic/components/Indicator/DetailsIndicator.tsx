ceafde877b81857537fd4ff2615955ee2278f6e3
Joel Keyser <keyser.joel@gmail.com>
2023-12-05 10:50:22 -0600

    touchup: indicate details if has claims

--- src/web/topic/components/Indicator/DetailsIndicator.tsx
+++ src/web/topic/components/Indicator/DetailsIndicator.tsx
@@ -1,6 +1,7 @@
 import { Article, ArticleOutlined } from "@mui/icons-material";
 
 import { setSelectedGraphPart } from "../../store/actions";
+import { useExplicitClaimCount } from "../../store/graphPartHooks";
 import { GraphPart } from "../../utils/graph";
 import { viewDetails } from "../Surface/TopicPane";
 import { Indicator } from "./Indicator";
@@ -10,7 +11,12 @@ interface Props {
 }
 
 export const DetailsIndicator = ({ graphPart }: Props) => {
-  const Icon = graphPart.data.notes.length > 0 ? Article : ArticleOutlined;
+  const explicitClaimCount = useExplicitClaimCount(graphPart.id);
+
+  const hasNotes = graphPart.data.notes.length > 0;
+  const hasClaims = explicitClaimCount > 0; // TODO: add separate indicator for claims
+  const hasDetails = hasNotes || hasClaims;
+  const Icon = hasDetails ? Article : ArticleOutlined;
 
   return (
     <Indicator
