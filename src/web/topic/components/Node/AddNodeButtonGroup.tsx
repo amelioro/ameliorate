f4d8580b9a746a4c3739bee4c17a73c429766403
Joel Keyser <keyser.joel@gmail.com>
2023-12-15 11:50:25 -0600

    perf: memoize add node button group
    performance was a bit slow with a lot of nodes and
    unrestricted editing.
    add node button group shouldn't need to re-render ever anyway

--- src/web/topic/components/Node/AddNodeButtonGroup.tsx
+++ src/web/topic/components/Node/AddNodeButtonGroup.tsx
@@ -1,4 +1,5 @@
 import { ButtonGroup } from "@mui/material";
+import { memo } from "react";
 
 import { NodeType, topicNodeTypes } from "../../../../common/node";
 import { useUnrestrictedEditing } from "../../../view/actionConfigStore";
@@ -15,46 +16,46 @@ interface Props {
   orientation: Orientation;
 }
 
-export const AddNodeButtonGroup = ({
-  className,
-  fromNodeId,
-  fromNodeType,
-  as,
-  orientation,
-}: Props) => {
-  const unrestrictedEditing = useUnrestrictedEditing();
+const AddNodeButtonGroup = memo(
+  ({ className, fromNodeId, fromNodeType, as, orientation }: Props) => {
+    const unrestrictedEditing = useUnrestrictedEditing();
 
-  const addableRelations: { toNodeType: NodeType; relation: Relation }[] =
-    unrestrictedEditing && topicNodeTypes.includes(fromNodeType)
-      ? // if unrestricted, allow adding any topic node as parent or child (shouldn't be very useful to have outside of topic nodes)
-        topicNodeTypes.map((nodeType) => ({
-          toNodeType: nodeType,
-          relation: {
-            child: as === "child" ? nodeType : fromNodeType,
-            name: "relatesTo",
-            parent: as === "parent" ? nodeType : fromNodeType,
-          },
-        }))
-      : addableRelationsFrom(fromNodeType, as);
+    const addableRelations: { toNodeType: NodeType; relation: Relation }[] =
+      unrestrictedEditing && topicNodeTypes.includes(fromNodeType)
+        ? // if unrestricted, allow adding any topic node as parent or child (shouldn't be very useful to have outside of topic nodes)
+          topicNodeTypes.map((nodeType) => ({
+            toNodeType: nodeType,
+            relation: {
+              child: as === "child" ? nodeType : fromNodeType,
+              name: "relatesTo",
+              parent: as === "parent" ? nodeType : fromNodeType,
+            },
+          }))
+        : addableRelationsFrom(fromNodeType, as);
 
-  if (addableRelations.length === 0) return <></>;
+    if (addableRelations.length === 0) return <></>;
 
-  return (
-    <ButtonGroup
-      variant="contained"
-      aria-label="add node button group"
-      className={className}
-      orientation={orientation === "DOWN" ? "horizontal" : "vertical"}
-    >
-      {addableRelations.map(({ toNodeType, relation }) => (
-        <AddNodeButton
-          key={toNodeType}
-          fromPartId={fromNodeId}
-          as={as}
-          toNodeType={toNodeType}
-          relation={relation}
-        />
-      ))}
-    </ButtonGroup>
-  );
-};
+    return (
+      <ButtonGroup
+        variant="contained"
+        aria-label="add node button group"
+        className={className}
+        orientation={orientation === "DOWN" ? "horizontal" : "vertical"}
+      >
+        {addableRelations.map(({ toNodeType, relation }) => (
+          <AddNodeButton
+            key={toNodeType}
+            fromPartId={fromNodeId}
+            as={as}
+            toNodeType={toNodeType}
+            relation={relation}
+          />
+        ))}
+      </ButtonGroup>
+    );
+  }
+);
+
+// eslint-disable-next-line functional/immutable-data
+AddNodeButtonGroup.displayName = "AddNodeButtonGroup";
+export { AddNodeButtonGroup };
