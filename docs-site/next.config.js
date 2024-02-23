57e40e1a6369ccddbd0c4cc938fd75fbd03d105c
Joel Keyser <keyser.joel@gmail.com>
2024-02-23 14:12:52 -0600

    fix(docs-site): build errors
    add eslint config and packages
    remove esm usage from next config

--- docs-site/next.config.js
+++ docs-site/next.config.js
@@ -1,12 +1,10 @@
-// eslint-disable-next-line import/default -- idk still works
-import nextra from "nextra";
-
-const withNextra = nextra({
+const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
 });
 
-export default withNextra({
+// eslint-disable-next-line functional/immutable-data -- when trying to use ESM, nextra encounters error "The requested module 'remark-reading-time' does not provide an export named 'default'"
+module.exports = withNextra({
   basePath: "/docs",
   images: {
     remotePatterns: [
