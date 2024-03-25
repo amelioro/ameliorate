3b6bd9a89d3c017c81c7bce1a5d3ab748fe2b900
Joel Keyser <keyser.joel@gmail.com>
2024-03-25 16:02:02 -0500

    fix: auth0 code still being added after login

--- src/pages/api/auth/[...auth0].page.ts
+++ src/pages/api/auth/[...auth0].page.ts
@@ -3,24 +3,33 @@ import { UserProfile } from "@auth0/nextjs-auth0/client";
 
 import { xprisma } from "../../../db/extendedPrisma";
 
-const afterCallback: AfterCallback = async (_req, res, session, _state) => {
+const afterCallback: AfterCallback = async (_req, res, session, state) => {
   const userClaims = session.user as UserProfile;
   if (!userClaims.sub) throw new Error("No sub claim in user profile");
 
   // fetch user from db
   const user = await xprisma.user.findUnique({ where: { authId: userClaims.sub } });
 
-  // Add any param to location so that netlify doesn't carry forward callback params with our redirect. See https://github.com/auth0/nextjs-auth0/issues/747#issuecomment-1183733931
-  // There should be an easier way to just remove the ?code and ?state params from oauth, but this works for now.
-  const params = "?login-success";
+  const returnTo = state?.returnTo as string | undefined;
 
   // redirect to /choose-username if user doesn't exist, otherwise let `returnTo` return to the page the user was last on
-  if (!user) {
-    const location = "/choose-username";
-    // causes server-client mismatch because this callback only runs on client, and `Login` button is rendered serverside-first
-    // seems ok.
-    res.setHeader("Location", `${location}${params}`);
-  }
+  // default to user's profile page
+  const path = user
+    ? returnTo
+      ? new URL(returnTo).pathname
+      : `/${user.username}`
+    : "/choose-username";
+
+  // Add any param to location so that netlify doesn't carry forward callback params with our redirect. See https://github.com/auth0/nextjs-auth0/issues/747#issuecomment-1183733931
+  // There should be an easier way to just remove the ?code and ?state params from oauth, but this works for now.
+  const params = user
+    ? returnTo
+      ? new URL(returnTo).search || "?login-success"
+      : "?login-success"
+    : "?login-success";
+
+  const location = `${path}${params}`;
+  res.setHeader("Location", location);
 
   return session;
 };
