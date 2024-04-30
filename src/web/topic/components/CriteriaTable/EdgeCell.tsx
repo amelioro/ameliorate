37d78c96d14f81435b8b62ac83c86b75a0d135c5
Joel Keyser <keyser.joel@gmail.com>
2024-04-30 13:22:56 -0500

    build: add tailwindcss
    want to try it out, seems useful particularly for quick, low-css usages.
    seems like MUI styled components + tailwind might be ok together,
    but we'll see as we try it more.

--- src/web/topic/components/CriteriaTable/EdgeCell.tsx
+++ src/web/topic/components/CriteriaTable/EdgeCell.tsx
@@ -1,25 +1,12 @@
-import styled from "@emotion/styled";
-
 import { Edge } from "../../utils/graph";
 import { CommonIndicators } from "../Indicator/CommonIndicators";
 import { ContentIndicators } from "../Indicator/ContentIndicators";
 
 export const EdgeCell = ({ edge }: { edge: Edge }) => {
   return (
-    <StyledDiv>
+    <div className="flex h-full items-center justify-center">
       <CommonIndicators graphPartId={edge.id} notes={edge.data.notes} />
-      <StyledContentIndicators graphPartId={edge.id} color="paper" />
-    </StyledDiv>
+      <ContentIndicators className="ml-0" graphPartId={edge.id} color="paper" />
+    </div>
   );
 };
-
-const StyledContentIndicators = styled(ContentIndicators)`
-  margin-left: 0;
-`;
-
-const StyledDiv = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-`;
