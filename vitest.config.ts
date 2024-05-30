07e0be31e752b7792515f833071403f3ce05960f
Joel Keyser <keyser.joel@gmail.com>
2024-05-29 19:44:00 -0500

    fix: vitest not using absolute paths
    also after using absolute paths, it started picking up tests in
    local .netlify/*, so `include` was added to only look at src/

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,10 @@
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
+    include: ["./src/**/*.{test,spec}.?(c|m)[jt]s?(x)"], // default from https://vitest.dev/config/#include but looking only at src/
     setupFiles: ["./scripts/setupTests.ts"],
   },
 });
