b9cfca5f527240e6d32c25a01c91885066dc8b29
Joel Keyser <keyser.joel@gmail.com>
2024-06-26 15:30:48 -0500

    touchup: add option for forceShownIndicator
    not sure if it's best to have toggle per indicator like this,
    but we'll keep an eye on the UX of it.
    it's nice to have off for reducing clutter and just looking at the
    diagram, but nice to have on when modifying the view, to easily
    understand the filtering.

--- src/web/topic/components/Indicator/StatusIndicators.tsx
+++ src/web/topic/components/Indicator/StatusIndicators.tsx
@@ -2,6 +2,7 @@ import { type ButtonProps, Stack } from "@mui/material";
 import { memo } from "react";
 
 import { ForceShownIndicator } from "@/web/topic/components/Indicator/ForceShownIndicator";
+import { useIndicateWhenNodeForcedToShow } from "@/web/view/userConfigStore";
 
 interface Props {
   graphPartId: string;
@@ -13,9 +14,13 @@ interface Props {
  * Future: e.g. controversial, hot, solid
  */
 const StatusIndicatorsBase = ({ graphPartId, color, className }: Props) => {
+  const indicateWhenNodeForcedToShow = useIndicateWhenNodeForcedToShow();
+
   return (
     <Stack direction="row" margin="2px" spacing="2px" className={className}>
-      <ForceShownIndicator nodeId={graphPartId} partColor={color} />
+      {indicateWhenNodeForcedToShow && (
+        <ForceShownIndicator nodeId={graphPartId} partColor={color} />
+      )}
     </Stack>
   );
 };
