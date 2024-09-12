0facc834044f00550b8b06e34e15527173af9122
Joel Keyser <keyser.joel@gmail.com>
2024-09-12 09:48:05 -0500

    fix: some homepage urls are broken
    was incorrectly using `path.join`, which is not intended for URLs.

--- src/web/common/urls.ts
+++ src/web/common/urls.ts
@@ -1,14 +1,13 @@
-import path from "path";
-
 export const examplesPage = "/examples";
 
 export const docsPage = "https://ameliorate.app/docs";
-export const discourseSessionsPage = path.join(docsPage, "/discourse-sessions");
-export const gettingStartedPage = path.join(docsPage, "/getting-started");
+// would be nice if there were an easy way to join these; `path.join` doesn't work well with "https://", and `new URL` doesn't allow appending to a basepath with subdirectories
+export const discourseSessionsPage = docsPage + "/discourse-sessions";
+export const gettingStartedPage = docsPage + "/getting-started";
 
 export const githubRepo = "https://github.com/amelioro/ameliorate";
-export const contributingPage = path.join(githubRepo, "/blob/main/CONTRIBUTING.md");
-export const feedbackPage = path.join(githubRepo, "/blob/main/CONTRIBUTING.md#providing-feedback");
+export const contributingPage = githubRepo + "/blob/main/CONTRIBUTING.md";
+export const feedbackPage = githubRepo + "/blob/main/CONTRIBUTING.md#providing-feedback";
 
 export const celebrateGif =
   "https://github.com/user-attachments/assets/e87009a0-4423-4406-b2f1-5a161d620fbb";
