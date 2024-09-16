2bedfc2127b961d7472ca6802bd9001102f20864
Joel Keyser <keyser.joel@gmail.com>
2024-09-16 16:12:38 -0500

    chore: log request urls for netlify functions

---
+++ src/middleware.page.ts
@@ -0,0 +1,9 @@
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  // log when we're handling a request, but filter out _next requests because we mostly care about fetches
+  // this is intended to aid with debugging requests, since it seems that netlify functions don't automatically log request URLs
+  if (!request.nextUrl.pathname.startsWith("/_next")) {
+    console.info("[request]", request.nextUrl.pathname);
+  }
+}
