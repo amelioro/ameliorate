66dc16f557dfcba77979c68b299a6fb731cd2bfa
Joel Keyser <keyser.joel@gmail.com>
2024-09-16 16:26:17 -0500

    chore: debug

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  console.log("middleware running");
+
   // log when we're handling a request, but filter out _next requests because we mostly care about fetches
   // this is intended to aid with debugging requests, since it seems that netlify functions don't automatically log request URLs
   if (!request.nextUrl.pathname.startsWith("/_next")) {
