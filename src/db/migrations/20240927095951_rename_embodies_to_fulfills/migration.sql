8b2b884fbd96131fd18841b60cbb5e7a07dbb73e
Joel Keyser <keyser.joel@gmail.com>
2024-09-27 10:20:18 -0500

    db: rename "embodies" -> "fulfills"
    the front-facing (i.e. labels) rename was done a while ago,
    but this updates all the not-front-facing code to match.

---
+++ src/db/migrations/20240927095951_rename_embodies_to_fulfills/migration.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+-- AlterEnum
+CREATE TYPE "EdgeType_intermediate" AS ENUM ('causes', 'subproblemOf', 'addresses', 'accomplishes', 'contingencyFor', 'createdBy', 'has', 'criterionFor', 'creates', 'fulfills', 'embodies', 'obstacleOf', 'asksAbout', 'potentialAnswerTo', 'relevantFor', 'sourceOf', 'mentions', 'supports', 'critiques', 'relatesTo');
+ALTER TABLE "edges" ALTER COLUMN "type" TYPE "EdgeType_intermediate" USING ("type"::text::"EdgeType_intermediate");
+ALTER TYPE "EdgeType" RENAME TO "EdgeType_old";
+ALTER TYPE "EdgeType_intermediate" RENAME TO "EdgeType";
+DROP TYPE "EdgeType_old";
+
+UPDATE "edges" SET "type" = 'fulfills' WHERE "type" = 'embodies';
+
+CREATE TYPE "EdgeType_new" AS ENUM ('causes', 'subproblemOf', 'addresses', 'accomplishes', 'contingencyFor', 'createdBy', 'has', 'criterionFor', 'creates', 'fulfills', 'obstacleOf', 'asksAbout', 'potentialAnswerTo', 'relevantFor', 'sourceOf', 'mentions', 'supports', 'critiques', 'relatesTo');
+ALTER TABLE "edges" ALTER COLUMN "type" TYPE "EdgeType_new" USING ("type"::text::"EdgeType_new");
+ALTER TYPE "EdgeType" RENAME TO "EdgeType_old";
+ALTER TYPE "EdgeType_new" RENAME TO "EdgeType";
+DROP TYPE "EdgeType_old";
+
+COMMIT;
