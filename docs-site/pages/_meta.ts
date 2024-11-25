0d5bf6431d25bd54f1aadf06526a7c7d54d33837
Joel Keyser <keyser.joel@gmail.com>
2024-11-25 15:04:27 -0600

    chore: upgrade nextra 2.13.3 -> 3.2.4
    includes installing dompurify types directly because nextra->mermaid has
    a version conflict for them. should be able to remove these types once
    the packages fix it.
    
    includes installing react types because Head now requires using
    react code.

---
+++ docs-site/pages/_meta.ts
@@ -0,0 +1,20 @@
+const meta = {
+  index: "Introduction",
+  comparisons: "Comparisons",
+  "-- App": {
+    type: "separator",
+    title: "App",
+  },
+  "getting-started": "",
+  features: "Features 🚧",
+  "diagramming-choices": "Diagramming Choices",
+  "-- Project": {
+    type: "separator",
+    title: "Project",
+  },
+  "discourse-sessions": "",
+  "release-status": "",
+  faq: "FAQ",
+};
+
+export default meta;
