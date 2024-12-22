f82829c5da853e29082b11ac192130d0f63f8bbe
Joel Keyser <keyser.joel@gmail.com>
2024-12-22 12:21:37 -0600

    chore: remove storybook
    wasn't really using this, it's just being an extra dependency.
    
    if we want to use storybook, we'll want to start from scratch anyway,
    since the current usage is outdated.
    
    includes removing legacy-peer-deps config because it was added for
    storybook. emotion and eslint happened to be depending on this too,
    so those versions were loosened to not require it anymore.

--- src/web/topic/components/Score/Score.stories.tsx
+++
@@ -1,7 +0,0 @@
-import { type Meta, type StoryFn } from "@storybook/react";
-
-import { Score } from "@/web/topic/components/Score/Score";
-
-export default { component: Score } as Meta<typeof Score>;
-
-export const Basic: StoryFn<typeof Score> = () => <Score graphPartId="1" />;
