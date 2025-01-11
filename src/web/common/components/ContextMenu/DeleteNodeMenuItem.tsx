297b0169dc96afcfd865cc40dd1cfcafd8cf9ab9
Artem Fivko <a.fivko@gmail.com>
2025-01-11 05:46:27 +0000

    feat(auth): Added hotkey for deleting nodes and fixed Auth0 setup

--- src/web/common/components/ContextMenu/DeleteNodeMenuItem.tsx
+++ src/web/common/components/ContextMenu/DeleteNodeMenuItem.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { ContextMenuItem } from "@/web/common/components/ContextMenu/CloseOnClickMenuItem";
 import { useSessionUser } from "@/web/common/hooks";
 import { deleteNode } from "@/web/topic/store/createDeleteActions";
@@ -7,6 +9,20 @@ import { Node } from "@/web/topic/utils/graph";
 export const DeleteNodeMenuItem = ({ node }: { node: Node }) => {
   const { sessionUser } = useSessionUser();
   const userCanEditTopicData = useUserCanEditTopicData(sessionUser?.username);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Delete" && userCanEditTopicData) {
+        deleteNode(node.id);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [node, userCanEditTopicData]);
 
   if (!userCanEditTopicData) return <></>;
 
