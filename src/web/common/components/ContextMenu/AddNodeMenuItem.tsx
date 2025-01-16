0a2e3b3b02e1453fd896dd9137328f9640334a6b
Roy Badell <145716905+rdubbi@users.noreply.github.com>
2025-01-16 16:54:06 -0500

    touchup(add node menu): applied icons and background colors for UX (#633)
    * Update src/web/common/components/ContextMenu/AddNodeMenuItem.tsx
    
    cleaning up coder comments.
    
    Co-authored-by: Joel Keyser <keyser.joel@gmail.com>
    
    * Update README.md
    
    bot will fix my contribution credits!
    
    Co-authored-by: Joel Keyser <keyser.joel@gmail.com>
    
    * Update src/web/common/components/ContextMenu/AddNodeMenuItem.tsx
    
    Changing to Tailwind inline styling
    
    Co-authored-by: Joel Keyser <keyser.joel@gmail.com>
    
    * Removed Manual Contribution
    
    Per suggestion, removed to allow bot to make list my contribution automatically.
    
    * Update AddNodeMenuItem.tsx
    
    Incorporated code to color the background of the icon instead of the whole menu item, as discussed.
    
    * fix: formatting
    
    ---------
    
    Co-authored-by: Joel Keyser <keyser.joel@gmail.com>

--- src/web/common/components/ContextMenu/AddNodeMenuItem.tsx
+++ src/web/common/components/ContextMenu/AddNodeMenuItem.tsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@mui/material/styles";
 import { NestedMenuItem } from "mui-nested-menu";
 
 import { breakdownNodeTypes, researchNodeTypes } from "@/common/node";
@@ -22,6 +23,8 @@ export const AddNodeMenuItem = ({ parentMenuOpen }: Props) => {
   // always showing breakdown + research nodes seems reasonable. justification nodes never need to be added on their own.
   const shownNodeTypes = breakdownNodeTypes.concat(researchNodeTypes);
 
+  const theme = useTheme();
+
   // shouldn't be able to view this menu item if we're in the table view
   if (!userCanEditTopicData || format == "table") return <></>;
 
@@ -33,11 +36,18 @@ export const AddNodeMenuItem = ({ parentMenuOpen }: Props) => {
         // match default mui menu padding and size
         className="px-[16px] [&_p]:px-0 [&_p]:text-sm"
       >
-        {shownNodeTypes.map((type) => (
-          <ContextMenuItem key={type} onClick={() => addNodeWithoutParent(type, "diagram")}>
-            {nodeDecorations[type].title}
-          </ContextMenuItem>
-        ))}
+        {shownNodeTypes.map((type) => {
+          const { NodeIcon, title } = nodeDecorations[type];
+          return (
+            <ContextMenuItem key={type} onClick={() => addNodeWithoutParent(type, "diagram")}>
+              <NodeIcon
+                sx={{ backgroundColor: theme.palette[type].main }}
+                className="mr-2 rounded p-0.5"
+              />
+              {title}
+            </ContextMenuItem>
+          );
+        })}
       </NestedMenuItem>
     </>
   );
