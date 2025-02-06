e9c3a7f425f6790043a0df947043acd3f1a25278
Joel Keyser <keyser.joel@gmail.com>
2025-02-05 18:06:28 -0600

    touchup(landing): use tabs for features section
    looks cleaner than selectable cards
    
    also this doesn't require mobile-specific css anymore

--- src/web/home/FeaturesSection.tsx
+++ src/web/home/FeaturesSection.tsx
@@ -1,51 +1,25 @@
-import { Link, Typography } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Link, Tab, Typography } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
 
-import { Card } from "@/web/home/Card";
+type Tab = "tradeoffs" | "views" | "flashlight" | "disagree";
 
-export const FeaturesSection = () => {
-  const [selectedCard, setSelectedCard] = useState<
-    "tradeoffs" | "views" | "flashlight" | "disagree"
-  >("tradeoffs");
-
-  const cards = (
-    <>
-      <Card
-        title="Summarize tradeoffs between solutions"
-        description="Show how important you think each tradeoff is, and how well each solution fulfills that tradeoff."
-        onClick={() => setSelectedCard("tradeoffs")}
-        selected={selectedCard === "tradeoffs"}
-      />
-      <Card
-        title="Focus on different aspects"
-        description="Create views to highlight specific parts of the diagram."
-        onClick={() => setSelectedCard("views")}
-        selected={selectedCard === "views"}
-      />
-      <Card
-        title="Move through complex diagrams"
-        description="Use flashlight mode to easily explore large diagrams."
-        onClick={() => setSelectedCard("flashlight")}
-        selected={selectedCard === "flashlight"}
-      />
-      <Card
-        title="Quickly identify where you disagree"
-        description="Compare scores to efficiently understand where your opinions lie in relation to others."
-        onClick={() => setSelectedCard("disagree")}
-        selected={selectedCard === "disagree"}
-      />
-    </>
-  );
-
-  const carouselItems = (
-    <>
-      <div className={selectedCard === "tradeoffs" ? "" : "hidden"}>
+const copy = {
+  tradeoffs: {
+    title: "Summarize tradeoffs between solutions",
+    description:
+      "Show how important you think each tradeoff is, and how well each solution fulfills that tradeoff.",
+    content: (
+      <div>
         <Image
           src="https://github.com/user-attachments/assets/6733473d-9122-4199-9e2d-d6d651f4a846"
           alt="criteria tables of cars-going-too-fast and ORM topics"
           width={1221}
           height={787}
+          unoptimized
+          // extra padding & bg because spacing seems to look better
+          className="rounded-xl border bg-paperPlain-main p-1 shadow"
         />
         <Typography variant="caption">
           Top-left:{" "}
@@ -64,13 +38,21 @@ export const FeaturesSection = () => {
           </Link>
         </Typography>
       </div>
-      <div className={selectedCard === "views" ? "" : "hidden"}>
+    ),
+  },
+  views: {
+    title: "Focus on different aspects",
+    description: "Create views to highlight specific parts of the diagram.",
+    content: (
+      <div>
         <Image
           src="https://github.com/user-attachments/assets/de5d9ebf-1b66-42f7-b1c5-cb0c0f595ee3"
           alt="clicking between views in cars-going-too-fast topic"
           width={1018}
           height={601}
           unoptimized // warning without this - gifs aren't optimized by nextjs apparently
+          // extra padding & bg because spacing seems to look better
+          className="rounded-xl border bg-paperPlain-main p-2 shadow"
         />
         <Typography variant="caption">
           Topic:{" "}
@@ -79,13 +61,20 @@ export const FeaturesSection = () => {
           </Link>
         </Typography>
       </div>
-      <div className={selectedCard === "flashlight" ? "" : "hidden"}>
+    ),
+  },
+  flashlight: {
+    title: "Move through complex diagrams",
+    description: "Use flashlight mode to easily explore large diagrams.",
+    content: (
+      <div>
         <Image
           src="https://github.com/user-attachments/assets/98d75b2b-6ca4-41cd-9322-314c75126232"
           alt="using flashlight mode in brutality-sugar topic"
           width={1022}
           height={728}
           unoptimized // warning without this - gifs aren't optimized by nextjs apparently
+          className="rounded-xl border shadow"
         />
         <Typography variant="caption">
           Topic:{" "}
@@ -94,13 +83,21 @@ export const FeaturesSection = () => {
           </Link>
         </Typography>
       </div>
-      <div className={selectedCard === "disagree" ? "" : "hidden"}>
+    ),
+  },
+  disagree: {
+    title: "Quickly identify where you disagree",
+    description:
+      "Compare scores to efficiently understand where your opinions lie in relation to others.",
+    content: (
+      <div>
         <Image
           src="https://github.com/user-attachments/assets/c1c9043a-4a0f-4af6-a309-ab3574301054"
           alt="comparing scores in cars-going-too-fast topic"
           width={756}
           height={703}
           unoptimized // warning without this - gifs aren't optimized by nextjs apparently
+          className="rounded-xl border shadow"
         />
         <Typography variant="caption">
           Topic:{" "}
@@ -109,32 +106,61 @@ export const FeaturesSection = () => {
           </Link>
         </Typography>
       </div>
-    </>
-  );
+    ),
+  },
+};
+
+export const FeaturesSection = () => {
+  const [selectedCard, setSelectedCard] = useState<Tab>("tradeoffs");
 
   return (
     <div className="flex flex-col text-center">
-      <Typography variant="h4">Features</Typography>
-      <Typography variant="body1">
-        Check out the{" "}
-        <Link href="https://ameliorate.app/docs" target="_blank">
-          docs
-        </Link>{" "}
-        to learn more.
-      </Typography>
+      <Typography variant="h4">Some cool features</Typography>
 
-      <div className="mt-2 hidden grid-cols-3 items-center gap-3 sm:grid">
-        <div className="flex flex-col gap-2 text-start [&_p]:hidden lg:[&_p]:block">{cards}</div>
-        {/* aspect ratio to keep different-sized images from jumping around */}
-        <div className="col-span-2 flex aspect-square items-center ">{carouselItems}</div>
-      </div>
+      <TabContext value={selectedCard}>
+        <TabList onChange={(_, value: Tab) => setSelectedCard(value)} variant="fullWidth">
+          <Tab label={copy.tradeoffs.title} value="tradeoffs" wrapped />
+          <Tab label={copy.views.title} value="views" wrapped />
+          <Tab label={copy.flashlight.title} value="flashlight" wrapped />
+          <Tab label={copy.disagree.title} value="disagree" wrapped />
+        </TabList>
 
-      <div className="mt-4 flex flex-col gap-3 sm:hidden">
-        <div className="flex h-32 overflow-x-auto text-start *:w-60 *:shrink-0 [&_p]:hidden">
-          {cards}
-        </div>
-        <div className="flex">{carouselItems}</div>
-      </div>
+        <TabPanel
+          key="tradeoffs"
+          value="tradeoffs"
+          className="flex flex-col items-center gap-2 p-0"
+        >
+          <Typography variant="body1" className="pt-3">
+            {copy.tradeoffs.description}
+          </Typography>
+          {copy.tradeoffs.content}
+        </TabPanel>
+
+        <TabPanel key="views" value="views" className="flex flex-col items-center gap-2 p-0">
+          <Typography variant="body1" className="pt-3">
+            {copy.views.description}
+          </Typography>
+          {copy.views.content}
+        </TabPanel>
+
+        <TabPanel
+          key="flashlight"
+          value="flashlight"
+          className="flex flex-col items-center gap-2 p-0"
+        >
+          <Typography variant="body1" className="pt-3">
+            {copy.flashlight.description}
+          </Typography>
+          {copy.flashlight.content}
+        </TabPanel>
+
+        <TabPanel key="disagree" value="disagree" className="flex flex-col items-center gap-2 p-0">
+          <Typography variant="body1" className="pt-3">
+            {copy.disagree.description}
+          </Typography>
+          {copy.disagree.content}
+        </TabPanel>
+      </TabContext>
     </div>
   );
 };
