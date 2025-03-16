d9de2431eb0fc80205bd877b209131dfe023a599
Joel Keyser <keyser.joel@gmail.com>
2025-03-16 14:39:51 -0500

    chore(docs-deps): upgrade to nextra 4
    this fixes the docs issues:
    - searchbar scrunched on small screens
    - banner does not close
    
    followed the migration guide https://the-guild.dev/blog/nextra-4 and
    nextra site's code as reference
    https://github.com/shuding/nextra/blob/main/docs.
    
    also includes adding `alt` to docs image tags because an error was being
    thrown otherwise.
    
    also upgrades next-plausible because old version wasn't compatible
    with next 15.

--- docs-site/next.config.js
+++ docs-site/next.config.js
@@ -1,9 +1,6 @@
 import nextra from "nextra";
 
-const withNextra = nextra({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-});
+const withNextra = nextra({});
 
 export default withNextra({
   basePath: "/docs", // bit annoying, but seems like this is needed in order to proxy the main app url/docs to here https://answers.netlify.com/t/support-guide-can-i-deploy-multiple-repositories-in-a-single-site/179
