ccc912a52eeede2def88b1d89a74d8b4d20afab3
Joel Keyser <keyser.joel@gmail.com>
2025-07-06 21:56:57 -0500

    touchup: add "add" buttons to core summary

--- src/web/summary/components/CoreNodesColumn.tsx
+++ src/web/summary/components/CoreNodesColumn.tsx
@@ -1,14 +1,23 @@
 import { Star } from "@mui/icons-material";
 
 import { Row } from "@/web/summary/components/Row";
+import { AddNodeButton } from "@/web/topic/components/Node/AddNodeButton";
 import { useCoreNodes } from "@/web/topic/diagramStore/nodeHooks";
+
+const AddButtons = (
+  // extra padding-bottom because core nodes have outline decoration that make them look closer to the buttons
+  <div className="pb-1.5">
+    <AddNodeButton toNodeType="problem" />
+    <AddNodeButton toNodeType="solution" />
+  </div>
+);
 
 export const CoreNodesColumn = () => {
   const coreNodes = useCoreNodes();
 
   return (
     <>
-      <Row title="Core Nodes" Icon={Star} nodes={coreNodes} />
+      <Row title="Core Nodes" Icon={Star} nodes={coreNodes} addButtonsSlot={AddButtons} />
     </>
   );
 };
--- src/web/topic/components/Node/AddNodeButton.tsx
+++ src/web/topic/components/Node/AddNodeButton.tsx
@@ -6,17 +6,32 @@ import { NodeType } from "@/common/node";
 import { useSessionUser } from "@/web/common/hooks";
 import { StyledButton } from "@/web/topic/components/Node/AddNodeButton.styles";
 import { WorkspaceContext } from "@/web/topic/components/TopicWorkspace/WorkspaceContext";
-import { addNode } from "@/web/topic/diagramStore/createDeleteActions";
+import { addNode, addNodeWithoutParent } from "@/web/topic/diagramStore/createDeleteActions";
 import { useUserCanEditTopicData } from "@/web/topic/topicStore/store";
 import { Relation } from "@/web/topic/utils/edge";
 import { type RelationDirection } from "@/web/topic/utils/graph";
 import { nodeDecorations } from "@/web/topic/utils/node";
 
-interface Props {
-  fromPartId: string;
-  as: RelationDirection;
+/**
+ * Either we add a node with a relation to an existing node, or we add a node without a parent.
+ *
+ * This is a little awkward, but I think we'll end up changing the UX of how we add nodes anyway
+ * (using a single plus button that opens a menu to each type to add) so this'll hopefully get refactored.
+ */
+type RelationProps =
+  | {
+      fromPartId: string;
+      as: RelationDirection;
+      relation: Relation;
+    }
+  | {
+      fromPartId?: undefined;
+      as?: undefined;
+      relation?: undefined;
+    };
+
+interface NonRelationProps {
   toNodeType: NodeType;
-  relation: Relation;
   /**
    * Generally want to select the new node to highlight it in the view, but some cases we want to
    * avoid changing selection so that the view isn't impacted as much (e.g. from the details pane)
@@ -32,7 +47,7 @@ export const AddNodeButton = ({
   relation,
   selectNewNode,
   className,
-}: Props) => {
+}: NonRelationProps & RelationProps) => {
   const { sessionUser } = useSessionUser();
   const userCanEditTopicData = useUserCanEditTopicData(sessionUser?.username);
   const context = useContext(WorkspaceContext);
@@ -40,6 +55,12 @@ export const AddNodeButton = ({
   if (!userCanEditTopicData) return <></>;
 
   const decoration = nodeDecorations[toNodeType];
+  const titleSuffix =
+    relation === undefined
+      ? ""
+      : as === "parent"
+        ? ` (this ${startCase(relation.child)} '${lowerCase(relation.name)}')`
+        : ` '${lowerCase(relation.name)}' this ${startCase(relation.parent)}`;
 
   return (
     <StyledButton
@@ -50,19 +71,15 @@ export const AddNodeButton = ({
       variant="contained"
       onClick={(event) => {
         event.stopPropagation(); // don't trigger selection of node
-        addNode({ fromPartId, as, toNodeType, relation, context, selectNewNode });
+
+        if (fromPartId === undefined) addNodeWithoutParent(toNodeType, context, selectNewNode);
+        else addNode({ fromPartId, as, toNodeType, relation, context, selectNewNode });
       }}
       // Not using MUI Tooltip because it throws anchorEl missing error when the button is hidden
       // after hovering it. Think we'd have to pass `show` into this component in order to hide
       // the tooltip at the same time as the button, rather than relying on css from the FlowNode,
       // but that'd be slightly annoying to do.
-      title={
-        `Add new ${decoration.title} (` +
-        (as === "parent"
-          ? `this ${startCase(relation.child)} '${lowerCase(relation.name)}'`
-          : `'${lowerCase(relation.name)}' this ${startCase(relation.parent)}`) +
-        `)`
-      }
+      title={`Add new ${decoration.title}` + titleSuffix}
       aria-label={`Add new ${decoration.title}`}
     >
       <decoration.NodeIcon />
