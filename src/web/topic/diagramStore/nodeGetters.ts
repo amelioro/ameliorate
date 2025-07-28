ee28e151ab0e1f9becfcc020dfd12087ffcfde3e
Joel Keyser <keyser.joel@gmail.com>
2025-07-28 15:54:31 -0500

    refactor: make diagram state helper for view store
    seems better to avoid the view store just grabbing the
    full diagram store state directly.

--- src/web/topic/diagramStore/nodeGetters.ts
+++ src/web/topic/diagramStore/nodeGetters.ts
@@ -1,7 +1,17 @@
 import { NodeType } from "@/common/node";
 import { useDiagramStore } from "@/web/topic/diagramStore/store";
+import { findNodeOrThrow } from "@/web/topic/utils/graph";
+import { neighbors } from "@/web/topic/utils/node";
 
 export const getDefaultNode = (nodeType: NodeType) => {
   const state = useDiagramStore.getState();
   return state.nodes.find((node) => node.type === nodeType);
+};
+
+export const getNeighbors = (nodeId: string) => {
+  const state = useDiagramStore.getState();
+
+  const node = findNodeOrThrow(nodeId, state.nodes);
+
+  return neighbors(node, state);
 };
