c0d8c3f02c25479c2193a91cf79e4cc3cca43325
Joel Keyser <keyser.joel@gmail.com>
2025-09-23 19:15:25 -0500

    touchup: use prisma-neon adapter
    apparently it's supposed to be better suited for serverless
    
    note: using prisma-pg locally though because prisma-neon is meant for
    connecting to neon, which we're typically not doing locally
    (and prisma engine requires _some_
    adapter)

--- src/db/basePrisma.ts
+++ src/db/basePrisma.ts
@@ -1,8 +1,19 @@
+import { neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaPg } from "@prisma/adapter-pg";
+import ws from "ws";
 
 import { PrismaClient } from "@/db/generated/prisma/client";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+// websocket is the default that neon's docs use, that's the main reason we're using it rather than http
+// eslint-disable-next-line functional/immutable-data
+neonConfig.webSocketConstructor = ws;
+
+const adapter =
+  // don't use neon adapter in development https://github.com/prisma/prisma/discussions/21346#discussioncomment-9068554
+  process.env.NODE_ENV !== "development"
+    ? new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+    : new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 // awkward solution to prevent many connections to the db, specifically as created by nextjs hotreloading
 // https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
