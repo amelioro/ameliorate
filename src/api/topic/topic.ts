51dd1c3e33cf72083ff2448a89585c303a359d11
Joel Keyser <keyser.joel@gmail.com>
2025-11-25 10:40:09 -0600

    refactor: extract findTopicByUsernameAndTitle
    so that we have a more consistent way of ensuring that we filter
    private topics out when querying.

--- src/api/topic/topic.ts
+++ src/api/topic/topic.ts
@@ -1,4 +1,5 @@
 import { xprisma } from "@/db/extendedPrisma";
+import { type TopicGetPayload, type TopicInclude } from "@/db/generated/prisma/models";
 
 export const getPublicTopics = async () => {
   return await xprisma.topic.findMany({
@@ -6,4 +7,25 @@ export const getPublicTopics = async () => {
       visibility: "public",
     },
   });
+};
+
+export const findTopicByUsernameAndTitle = async <TInclude extends TopicInclude | undefined>(
+  isCreator: boolean,
+  topicCreatorName: string,
+  topicTitle: string,
+  include?: TInclude,
+) => {
+  const topic = await xprisma.topic.findFirst({
+    where: {
+      title: topicTitle,
+      creatorName: topicCreatorName,
+      visibility: isCreator ? undefined : { not: "private" },
+    },
+    include,
+  });
+
+  // not sure how to more-cleanly (i.e. without "as") ensure the `include` type carries through `findFirst` when `include` can be undefined.
+  // when `include` can't be undefined, it works fine.
+  // apparently function overloads (creating a separate prototype for include being defined vs undefined) might work, but that seems annoying to write/read, and not worth
+  return topic as TopicGetPayload<{ include: TInclude }> | null;
 };
