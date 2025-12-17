3d569bf81d71063b13b880109bf4bcb6922b51ff
Joel Keyser <keyser.joel@gmail.com>
2025-12-17 10:59:19 -0600

    fix: mui v7 not integrated with Tailwind v4
    MUI styles needed to be layered because unlayered styles always
    take precedence over layered ones, and Tailwind v4 is layered now.
    
    might be able to clean up this setup by upgrading nextjs or
    moving to app router?
    
    includes:
    - integrating react-flow styles with Tailwind v4 (layering those styles)
    - adding `cursor: pointer` to all buttons since this was removed from
    Tailwind v4 preflight
    - moving legacy tailwind js config below @layer declarations because
    there was a "at-rule or selector expected" tailwindcss error?

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,4 +1,12 @@
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from "@mui/material-nextjs/v14-pagesRouter";
+import type { DocumentContext } from "next/document";
 import { Head, Html, Main, NextScript } from "next/document";
+
+import { clientEmotionCache } from "@/web/common/theme";
 
 // grab default font size from root <html> if we're on the client
 // thanks https://stackoverflow.com/a/49299352
@@ -20,10 +28,12 @@ export const htmlDefaultFontSize =
  */
 export const scalePxViaDefaultFontSize = (px: number) => px * (htmlDefaultFontSize / 16);
 
-export default function Document() {
+export default function Document(props: DocumentHeadTagsProps) {
   return (
     <Html>
-      <Head />
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -31,3 +41,11 @@ export default function Document() {
     </Html>
   );
 }
+
+// eslint-disable-next-line functional/immutable-data -- seems like mutation is the expected way to set `getInitialProps` https://mui.com/material-ui/integrations/tailwindcss/tailwindcss-v4/#next-js-pages-router
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx, {
+    emotionCache: clientEmotionCache,
+  });
+  return finalProps;
+};
