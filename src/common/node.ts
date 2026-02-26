e2e35a46c886d50308fed70f9dd1d765b9b94234
Joel Keyser <keyser.joel@gmail.com>
2026-02-26 09:10:24 -0600

    refactor: reactFlow->diagramStore EdgeSchema
    it's more accurate now that we're no longer passing everything
    into react flow.

--- src/common/node.ts
+++ src/common/node.ts
@@ -147,14 +147,16 @@ export const createNodeSchema = nodeSchema
 export type CreateNode = z.infer<typeof createNodeSchema>;
 
 /**
- * Ideally we wouldn't need this outside of the react-flow components, but we unfortunately let this
- * format leak into everywhere on the frontend, including the download topic JSON logic, and we use
- * downloaded files for `examples/`, which we use on the backend (e.g. for topic AI examples).
+ * Ideally we wouldn't need this outside of the frontend, or that it'd be identical to the backend
+ * schema, but the frontend doesn't need `topicId` so it seems like it'll at least be different in
+ * that way. Unfortunately we we need this in `common/` here because it's used for the download
+ * topic JSON logic, and we use downloaded files for `examples/` on the backend (e.g. for topic AI
+ * examples).
  *
- * TODO: use the above `nodeSchema` in most places on the frontend, and only use the flow schema for
- * flow-related components.
+ * TODO?: I think ideally we'd have this be identical to backend schema except for `topicId`, and
+ * name it something that implies `topicId` isn't needed ("localNodeSchema"?).
  */
-export const reactFlowNodeSchema = z.object({
+export const diagramStoreNodeSchema = z.object({
   id: z.string(),
   data: z.object({
     /**
