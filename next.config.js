9a86848f9606c05514faeab521d58b896769d8ec
Joel Keyser <keyser.joel@gmail.com>
2026-03-31 16:46:18 -0500

    chore: completely turn off sentry in development
    seems like it improves dev build times from 2.5s to 0.8s ?
    
    found while looking for ways to reduce /playground load times
    (though this doesn't help that)

--- next.config.js
+++ next.config.js
@@ -35,51 +35,57 @@ const nextConfig = {
 };
 
 /**
- * Note: `next dev --turbo` warns that webpack is configured because `withSentryConfig`
- * and `@next/bundle-analyzer` both add webpack hooks. That is expected for now:
- * this app is on `next@14.2.35`, where Turbopack is only used for local dev,
+ * Turn off sentry in development since it's usually not needed, and can slow down builds a few seconds.
+ *
+ * TODO?: ideally would separately have a sentryConfig but couldn't figure out how to get the `SentryBuildOptions` type
+ *
+ * Note: there's also a warning when we run `next dev --turbo`, saying that webpack is configured,
+ * because `withSentryConfig` and `@next/bundle-analyzer` both add webpack hooks. That is expected
+ * for now: this app is on `next@14.2.35`, where Turbopack is only used for local dev,
  * while `next build` still uses webpack. So the warning is dev-only and is not
  * affecting production builds. To remove it cleanly, we would need to move to a
  * Turbopack-compatible stack (`next@15.4.1+`, `@sentry/nextjs@10.13.0+`) and
  * replace webpack-only `@next/bundle-analyzer` with Turbopack's analyzer.
- * TODO?: ideally would separately have a sentryConfig but couldn't figure out how to get the `SentryBuildOptions` type
  */
-const nextAndSentryConfig = withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+const nextAndSentryConfig =
+  process.env.SENTRY_ENVIRONMENT === "development"
+    ? nextConfig
+    : withSentryConfig(nextConfig, {
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "amelioro",
-  project: "ameliorate",
+        org: "amelioro",
+        project: "ameliorate",
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+        // Only print logs for uploading source maps in CI
+        silent: !process.env.CI,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+        // For all available options, see:
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  // widenClientFileUpload: true,
+        // Upload a larger set of source maps for prettier stack traces (increases build time)
+        // widenClientFileUpload: true,
 
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
+        // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+        // This can increase your server load as well as your hosting bill.
+        // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+        // side errors will fail.
+        // tunnelRoute: "/monitoring",
 
-  // don't need sourcemaps uploaded to sentry in development; this process takes some time during build, so skip it if unnecessary
-  sourcemaps: { disable: process.env.SENTRY_ENVIRONMENT === "development" },
+        // don't need sourcemaps uploaded to sentry in development; this process takes some time during build, so skip it if unnecessary
+        sourcemaps: { disable: process.env.SENTRY_ENVIRONMENT === "development" },
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
+        // Hides source maps from generated client bundles
+        hideSourceMaps: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+        // Automatically tree-shake Sentry logger statements to reduce bundle size
+        disableLogger: true,
 
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  // automaticVercelMonitors: true,
-});
+        // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+        // See the following for more information:
+        // https://docs.sentry.io/product/crons/
+        // https://vercel.com/docs/cron-jobs
+        // automaticVercelMonitors: true,
+      });
 
 export default withBundleAnalyzer(nextAndSentryConfig);
